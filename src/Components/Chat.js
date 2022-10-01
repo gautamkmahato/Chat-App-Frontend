8b52c88c430ed51db2b3dc126abde08eead41e9a
gautamkmahato <gautamkmahato@gmail.com>
2022-10-01 13:48:45 +0530

    server url changed

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -3,7 +3,7 @@ import io from 'socket.io-client';
 import { useEffect, useState } from 'react';
 //import { v4 as uuidv4 } from 'uuid';
 
-const socket = io("http://localhost:5000/");
+const socket = io("https://chat-app-backend-nc06.onrender.com");
 
 function Chat() {
 
